2eeabf813c789cfa3a1d84983d97f6dc29e50d01
Filip <fivanusec@gmail.com>
2020-11-05 21:17:10 +0100

    Testing update 3
    ImportPictures module works, now need some kind of way to reimport pictures without shutting down full program, program recognises people from img folder.

--- main.py
+++ main.py
@@ -3,38 +3,6 @@ import numpy as np
 import face_recognition
 from ImportPictures import Pictures
 
-"""
-Jednostavni program za usporedbu lica 
-"""
-"""
-imgFilip = face_recognition.load_image_file('Slike/Test3.jpg')
-imgFilip = cv2.cvtColor(imgFilip,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('Slike/Test4.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
-
-faceLoc = face_recognition.face_locations(imgFilip)[0]
-encodeFilip = face_recognition.face_encodings(imgFilip)[0]
-cv2.rectangle(imgFilip,(faceLoc[3], faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
-
-faceLocTest = face_recognition.face_locations(imgTest)[0]
-encodeTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest,(faceLocTest[3], faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
-
-results = face_recognition.compare_faces([encodeFilip],encodeTest)
-faceDis = face_recognition.face_distance([encodeFilip], encodeTest)
-
-print(results, faceDis)
-
-cv2.imshow("Filip", imgFilip)
-cv2.imshow("Test", imgTest)
-cv2.waitKey(0)
-
-Prikaz
-cv2.imshow('Filip', imgFilip)
-cv2.imshow('Test', imgTest)
-cv2.waitKey(0)
-"""
-
 images = []
 classNames = [] 
 encodings = []
@@ -53,3 +21,38 @@ if(images == [] or classNames == [] or encodings == []):
         print("Encodings are empty")
 else:
     print("Images loaded successfully!")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurrFrame = face_recognition.face_locations(imgS)
+    encodesCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
+
+    for encodeFace, faceloc in zip(encodesCurrFrame, facesCurrFrame):
+        matches = face_recognition.compare_faces(encodings, encodeFace)
+        faceDis = face_recognition.face_distance(encodings, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x1,y2,x2 = faceloc
+            y1,x1,y2,x2 = y1*4,x1*4,y2*4,x2*4 
+            cv2.rectangle(img, (x1,y1), (x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0), cv2.FILLED)
+            cv2.putText(img, name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,0),2)
+        else:
+            y1,x1,y2,x2 = faceloc
+            y1,x1,y2,x2 = y1*4,x1*4,y2*4,x2*4 
+            cv2.rectangle(img, (x1,y1), (x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0), cv2.FILLED)
+            cv2.putText(img, "No match",(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,0),2)
+            print("No match")
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
