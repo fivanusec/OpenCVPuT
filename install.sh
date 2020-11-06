d3b94eca517b7c84ede4607ca93c1a7ace5bac39
Filip <fivanusec@gmail.com>
2020-11-06 09:47:18 +0100

    Testing update 4.1
    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+PS3='Select your linux flavor: '
+options=("Ubuntu/Debian" "Arch" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Ubuntu/Debian")
+            sudo apt install cmake
+            break
+            ;;
+        "Arch")
+            sudo pacman -S cmake
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
 pip install cmake
 pip install dlib
 pip install numpy
